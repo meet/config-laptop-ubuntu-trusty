2aab0419ba60160c7166152ae58a7d09d9bf2fcd
Eric Timmons <etimmons@mit.edu>
2015-07-04 13:28:11 -0400

    Ready for installation on laptops.

--- etc/profile.d/pip.sh
+++
@@ -1,6 +0,0 @@
-# Becuase Ubuntu is always out of date, 14.04's version of pip doesn't look for
-# a global config file (?!). This tells it to ignore user config and just use
-# the global config.
-
-export PIP_CONFIG_FILE=/etc/pip.conf
-
